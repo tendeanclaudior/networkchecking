491db88adb95b2c5707e3896352007a6e9cbc6ca
Dio Tendean <tendeanclaudio1234@gmail.com>
2023-05-23 15:13:12 +0800

    Initial Commit

---
+++ NetworkChecking/NetworkCheckingApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkCheckingApp.swift
+//  NetworkChecking
+//
+//  Created by Claudio Tendean on 23/05/23.
+//
+
+import SwiftUI
+
+@main
+struct NetworkCheckingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
